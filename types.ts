301cbd414dc138900d45f9f95fd53dd915a42470
mrsaleem4781 <mrsaleem4781@gmail.com>
2026-02-14 16:41:38 +0500

    Fixed merge conflicts in types.ts

--- types.ts
+++ types.ts
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-
-export enum PhotoSize {
-  PASSPORT_PAK = 'Pakistani Passport (2x2")',
-  PASSPORT_US = 'US Standard (2x2")',
-  PASSPORT_EU = 'EU/Global (35x45mm)',
-  CUSTOM = 'Custom Size'
-}
-
-export enum PaperSize {
-  A6 = 'A6 (105x148mm)',
-  A4 = 'A4 (210x297mm)'
-}
-
-export enum BgColor {
-  WHITE = '#FFFFFF',
-  LIGHT_BLUE = '#ADD8E6',
-  DARK_BLUE = '#003399',
-  RED = '#FF0000',
-  CUSTOM = 'custom'
-}
-
-export interface ProcessingOptions {
-  size: PhotoSize;
-  paperSize: PaperSize;
-  bgColor: string;
-  customColor: string;
-  copies: number;
-  enhance: boolean;
-  smoothness: number; // 0-100
-  sharpness: number;  // 0-100
-  customWidth: number;
-  customHeight: number;
-=======
 export enum PhotoSize {
   PASSPORT_PAK = "35x45mm",
   PASSPORT_US = "2x2in",
@@ -49,7 +15,6 @@ export enum BgColor {
   DARK_BLUE = "#00008b",
   RED = "#ff0000",
   CUSTOM = "custom"
->>>>>>> ca2656e613e63ed0ca06b1cde5fcaff34e7bfb7d
 }
 
 export interface PhotoState {
@@ -58,8 +23,6 @@ export interface PhotoState {
   isProcessing: boolean;
   error: string | null;
 }
-<<<<<<< HEAD
-=======
 
 export interface ProcessingOptions {
   size: PhotoSize;
@@ -73,4 +36,3 @@ export interface ProcessingOptions {
   customWidth?: number;
   customHeight?: number;
 }
->>>>>>> ca2656e613e63ed0ca06b1cde5fcaff34e7bfb7d
